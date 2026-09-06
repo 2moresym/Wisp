1846fbfa6278f49d93de507ab00f6adf90859500
Vas <vasquintas@gmail.com>
2026-09-05 19:28:49 -0500

    window: fix Wayland display proxy type

--- crates/wisp-window/src/wayland.rs
+++ crates/wisp-window/src/wayland.rs
@@ -135,7 +135,7 @@ impl WaylandWindow {
         Ok(())
     }
 
-    pub fn display(&self) -> &wayland_client::Display {
+    pub fn display(&self) -> &wayland_client::protocol::wl_display::WlDisplay {
         self.connection.display()
     }
 
