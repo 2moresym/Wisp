0df497cbd6d298dff4ae329e2605b97610c68626
2vex <2nosym@gmail.com>
2026-09-08 00:40:11 -0500

    cleanup: fix Win32 ABI address warnings

--- crates/wisp-runtime/src/win32.rs
+++ crates/wisp-runtime/src/win32.rs
@@ -129,24 +129,22 @@ unsafe extern "C" {
 
 #[inline]
 pub fn abi_address(symbol: &str) -> Option<u64> {
-    macro_rules! addr { ($name:literal, $fn_name:ident) => { if symbol.eq_ignore_ascii_case($name) { return Some($fn_name as usize as u64); } }; }
-    unsafe {
-        addr!("GetLastError", wisp_abi_GetLastError);
-        addr!("SetLastError", wisp_abi_SetLastError);
-        addr!("VirtualAlloc", wisp_abi_VirtualAlloc);
-        addr!("VirtualProtect", wisp_abi_VirtualProtect);
-        addr!("TlsAlloc", wisp_abi_TlsAlloc);
-        addr!("TlsFree", wisp_abi_TlsFree);
-        addr!("TlsGetValue", wisp_abi_TlsGetValue);
-        addr!("TlsSetValue", wisp_abi_TlsSetValue);
-        addr!("CreateThread", wisp_abi_CreateThread);
-        addr!("WaitForSingleObject", wisp_abi_WaitForSingleObject);
-        addr!("CloseHandle", wisp_abi_CloseHandle);
-        addr!("GetModuleHandleA", wisp_abi_GetModuleHandleA);
-        addr!("GetModuleHandleW", wisp_abi_GetModuleHandleW);
-        addr!("GetProcAddress", wisp_abi_GetProcAddress);
-        addr!("ExitProcess", wisp_abi_ExitProcess);
-    }
+    macro_rules! addr { ($name:literal, $fn_name:ident) => { if symbol.eq_ignore_ascii_case($name) { return Some($fn_name as *const () as usize as u64); } }; }
+    addr!("GetLastError", wisp_abi_GetLastError);
+    addr!("SetLastError", wisp_abi_SetLastError);
+    addr!("VirtualAlloc", wisp_abi_VirtualAlloc);
+    addr!("VirtualProtect", wisp_abi_VirtualProtect);
+    addr!("TlsAlloc", wisp_abi_TlsAlloc);
+    addr!("TlsFree", wisp_abi_TlsFree);
+    addr!("TlsGetValue", wisp_abi_TlsGetValue);
+    addr!("TlsSetValue", wisp_abi_TlsSetValue);
+    addr!("CreateThread", wisp_abi_CreateThread);
+    addr!("WaitForSingleObject", wisp_abi_WaitForSingleObject);
+    addr!("CloseHandle", wisp_abi_CloseHandle);
+    addr!("GetModuleHandleA", wisp_abi_GetModuleHandleA);
+    addr!("GetModuleHandleW", wisp_abi_GetModuleHandleW);
+    addr!("GetProcAddress", wisp_abi_GetProcAddress);
+    addr!("ExitProcess", wisp_abi_ExitProcess);
     None
 }
 
